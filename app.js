8e85a925cdd82d159cc9f0a84301986c8b6a807c
Dennis Wakahia <dwakahia@cytonn.com>
2022-01-13 16:58:25 +0300

    index html file add

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 const users = require('./routes/users')
+const path = require('path')
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send('its running');
+    res.sendFile(path.join(`${__dirname}/index.html`));
 })
 
 app.use('/users',users);
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,21 +16,22 @@ exports.createUser = (req, res, next) => {
 
 exports.getUsers = (req, res, next) => {
     res.status(200).json({
-        message: 'users list',
+        message: users.length > 0 ? 'users list' : 'No users found',
         users: users
     })
 }
 
 exports.getUserById = (req, res, next) => {
     let user = users.find((user) => user.id == req.params.id);
-    res.status(200).json({
-        message: 'user fetched',
+    res.status(user ? 200 : 404).json({
+        message: user ? 'user fetched' : 'no existing user with such Id',
         user: user
     })
 }
 
 exports.updateUser = (req, res, next) => {
     const {name, age, email, phone} = req.body;
+    let user = users.find((user) => user.id == req.params.id);
     let newUser = new User(req.params.id, name, age, email, phone);
     users = users.map((user) => {
         if (user.id == req.params.id) {
@@ -38,18 +39,15 @@ exports.updateUser = (req, res, next) => {
         }
         return user;
     });
-
-    res.status(200).json({
-        message: 'user updated successfully',
-        user: newUser
-    })
+    let response = user ? {message: 'user updated successfully', user: newUser} : {message: 'no existing user with such Id'}
+    res.status(user ? 200 : 404).json(response)
 }
 
 exports.deleteUser = (req, res, next) => {
     let removedUser = users.find((user) => user.id == req.params.id);
     users = users.filter((user) => user.id != req.params.id)
-    res.status(200).json({
-        message: 'user deleted successfully',
+    res.status(removedUser ? 200 : 404).json({
+        message: removedUser ? 'user deleted successfully' : 'no existing user with such Id',
         user: removedUser
     })
 
